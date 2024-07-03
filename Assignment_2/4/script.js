2002612e0552a5bc8495d9a4ee2f7dba7b1e783d
BibekSodari <080bct025@ioepc.edu.np>
2024-07-03 18:32:05 +0400

    Add files via upload

---
+++ Assignment_2/4/script.js
@@ -0,0 +1,11 @@
+const person = {
+    fname: 'Bibek',
+    age: 19,
+    city: 'Lahan'
+};
+
+const { fname, age, city } = person;
+
+console.log(`Name: ${fname}`);
+console.log(`Age: ${age}`);
+console.log(`City: ${city}`);
